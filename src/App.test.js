353ab7067fc8c82f8332848c23e92f4d116b9c7c
xiangming <xzeng0011@gmail.com>
2024-11-17 14:21:28 +1100

    fix(tests): remove unnecessary app test

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders app', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/this is my app/i);
-  expect(linkElement).toBeInTheDocument();
-});
